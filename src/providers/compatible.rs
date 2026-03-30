7dae4ef47b3e69856e6b9aaef771210044818edb
baiyu.byte <baiyu.byte@bytedance.com>
2026-03-30 14:34:17 +0800

    perf(providers): increase timeout duration from 120 to 600 seconds

--- src/providers/compatible.rs
+++ src/providers/compatible.rs
@@ -303,7 +303,7 @@ impl OpenAiCompatibleProvider {
             }
 
             let builder = Client::builder()
-                .timeout(std::time::Duration::from_secs(120))
+                .timeout(std::time::Duration::from_secs(10 * 60))
                 .connect_timeout(std::time::Duration::from_secs(10))
                 .default_headers(headers);
             let builder =
